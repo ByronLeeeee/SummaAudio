620a9d6e16506c03966957e0f17751a98308953a
ByronLeeeee <demonbyron@gmail.com>
2024-08-16 10:53:02 +0800

    refactor(scripts): 增加日志记录并改进错误处理
    在modelscope_scripts.py和utils.py中添加日志记录和改进错误处理。具体包括:
    - 在modelscope_scripts.py中引入并更新日志记录功能，特别是在主ASR流程中。
    - 改进modelscope_scripts.py中的错误处理，特别是在处理环境变量和模型调用时。
    - 更新utils.py中的setup_logger函数以包含文件日志记录和控制台日志记录。

--- scripts/modelscope_scripts.py
+++ scripts/modelscope_scripts.py
@@ -1,87 +1,151 @@
-from scripts.utils import load_config
+from scripts.utils import load_config,setup_logger
 from modelscope.utils.constant import Tasks
 from modelscope.pipelines import pipeline
 import sys
 import json
 import os
 import streamlit as st
+
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+logger = setup_logger('MODELSCOPE_SCRIPTS')
 
-def get_MODELSCOPE_config():
-    return load_config('MODELSCOPE')
+def get_MODELSCOPE_config(config):
+    return load_config('MODELSCOPE')[config]
 
 
-def recognition(audio_in, model, model_revision, vad_model, vad_model_revision, punc_model, punc_model_revision,spk_model,spk_model_revision) -> list:
-    os.environ["MODELSCOPE_CACHE"] = get_MODELSCOPE_config()[
-        "MODELSCOPE_CACHE"]
+def recognition(audio_in, model, model_revision, vad_model, vad_model_revision, punc_model, punc_model_revision, spk_model, spk_model_revision) -> list:
+    """
+    使用指定的模型对音频进行识别，并返回识别结果。
 
-    inference_pipeline = pipeline(
-        task=Tasks.auto_speech_recognition,
-        model=model,
-        model_revision=model_revision,
-        vad_model=vad_model, vad_model_revision=vad_model_revision,
-        punc_model=punc_model, punc_model_revision=punc_model_revision,
-        spk_model=spk_model, spk_model_revision=spk_model_revision,
+    :param audio_in: 输入的音频数据
+    :param model: 主模型名称
+    :param model_revision: 主模型版本
+    :param vad_model: 语音活动检测模型名称
+    :param vad_model_revision: 语音活动检测模型版本
+    :param punc_model: 标点符号预测模型名称
+    :param punc_model_revision: 标点符号预测模型版本
+    :param spk_model: 说话人识别模型名称
+    :param spk_model_revision: 说话人识别模型版本
+    :return: 识别结果列表
+    """
+    cache_path = get_MODELSCOPE_config('MODELSCOPE_CACHE')
+    if not cache_path:
+        raise ValueError("MODELSCOPE_CACHE is not configured.")
+    os.environ["MODELSCOPE_CACHE"] = cache_path
 
-    )
-    rec_result = inference_pipeline(audio_in)
+    try:
+        # 创建推理管道
+        inference_pipeline = pipeline(
+            task=Tasks.auto_speech_recognition,
+            model=model,
+            model_revision=model_revision,
+            vad_model=vad_model,
+            vad_model_revision=vad_model_revision,
+            punc_model=punc_model,
+            punc_model_revision=punc_model_revision,
+            spk_model=spk_model,
+            spk_model_revision=spk_model_revision,
+        )
 
-    return rec_result
+        # 执行识别
+        rec_result = inference_pipeline(audio_in)
+
+        return rec_result
+
+    except Exception as e:
+        # 异常处理
+        logger.error(f"Error occurred during recognition: {e}")
+        return []
 
 
 def organise_recognition(result: list):
+    """
+    根据语音识别结果组织文本和说话人信息。
+
+    :param result: 包含字典元素的列表，每个字典包含文本和句子详细信息。
+    
+    :return: 
+        - full_text: 识别出的完整文本。
+
+        - organised_text: 按照说话人组织的文本，每个说话人的文本单独显示。
+
+    :raises ValueError: 如果输入格式不正确或列表为空或列表元素不是字典。
+    """
     if not result or not isinstance(result[0], dict):
         raise ValueError(
             "Invalid input format: 'result' should be a non-empty list with dictionaries as elements.")
 
     result_info = result[0]
     full_text = result_info.get('text', "")
-    sentence_info = result_info.get('sentence_info', "")
+    sentence_details = result_info.get('sentence_info', "")
 
-    organise_text = ""
-    if sentence_info and isinstance(sentence_info, list):
-        current_speaker = None
-        current_text = ""
+    organised_text = ""
+    if sentence_details and isinstance(sentence_details, list):
+        previous_speaker = None
+        speaker_text = ""
 
-        for item in sentence_info:
-            if current_speaker is None or item['spk'] != current_speaker:
-                if current_speaker is not None:
-                    organise_text += f"说话人{str(current_speaker + 1)}: {current_text}\n\n"
-                current_speaker = item['spk']
-                current_text = item['text']
+        for segment in sentence_details:
+            speaker = segment['spk']
+            text = segment['text']
+
+            if previous_speaker is None or speaker != previous_speaker:
+                if previous_speaker is not None:
+                    organised_text += f"说话人{previous_speaker + 1}: {speaker_text}\n\n"
+                previous_speaker = speaker
+                speaker_text = text
             else:
-                current_text += "" + item['text']
+                speaker_text += text
+        if previous_speaker is not None:
+            organised_text += f"说话人{previous_speaker + 1}: {speaker_text}"
 
-        if current_speaker is not None:
-            organise_text += f"说话人{str(current_speaker + 1)}: {current_text}"
-
-    return full_text, organise_text
+    return full_text, organised_text
 
 
 def save_output_result(full_text: str, organise_text: str, output_file: str,mode='normal'):
+    """
+    保存输出结果。
 
+    根据不同模式（normal或summary）组织文本并保存到指定目录。
+
+    :param:
+        - full_text: str, 全文内容。
+
+        - organise_text: str, 根据模式组织后的文本内容。
+
+        - output_file: str, 输出文件的名称。
+
+        - mode: str, 模式，'normal' 表示分说话人模式，其他值表示归纳模式。
+
+    :return:
+        - bool
+    """
     try:
-        output_path = get_MODELSCOPE_config()['output_dir']
+        output_path = get_MODELSCOPE_config('output_dir')
+        
+        if not output_file or '/' in output_file or '\\' in output_file:
+            raise ValueError("Invalid output_file name")
 
-        if not os.path.exists(f'{output_path}/{output_file}'):
-            os.makedirs(f'{output_path}/{output_file}')
+        output_dir = os.path.join(output_path, output_file)
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
 
-        if full_text != "":
-            with open(f'{output_path}/{output_file}/全文.txt', 'w', encoding='utf-8') as f:
-                f.write(full_text)
+        txt_name = '分说话人' if mode == 'normal' else '归纳'
+        file_contents = {
+            "全文.txt": full_text,
+            f"{txt_name}.txt": organise_text
+        }
 
-        if organise_text != "":
-            if mode == 'normal':
-                txt_name = '分说话人'
-            if mode == 'one_click':
-                txt_name = '归纳'
-            with open(f'{output_path}/{output_file}/{txt_name}.txt', 'w', encoding='utf-8') as f:
-                f.write(organise_text)
+        for file_name, content in file_contents.items():
+            if content != "":
+                file_path = os.path.join(output_dir, file_name)
+                with open(file_path, 'w', encoding='utf-8') as f:
+                    f.write(content)
 
         return True
     except Exception as e:
-        raise f'Error: {e}'
+        logger.error(f"Failed to save output result: {e}")
+        return False
 
 def get_modelscope_model_list():
     with open("config/modelscope_models.json", "r") as f:
--- scripts/utils.py
+++ scripts/utils.py
@@ -2,6 +2,7 @@ import json
 import streamlit as st
 import pyperclip
 import configparser
+import logging
 
 
 JSON_PATH = 'config/prompts.json'
@@ -30,4 +31,20 @@ def load_config(section: str):
     config.read('config/config.ini', encoding='utf-8')
     return config[section]
 
+def setup_logger(name, log_file='logger/log.log', level=logging.INFO):
+    """Function to create a logging object and set the level."""
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 
+    handler = logging.FileHandler(log_file)  # 创建一个FileHandler，用于写到本地
+    handler.setFormatter(formatter)  # 设置文件日志格式
+
+    console_handler = logging.StreamHandler()  # 创建一个StreamHandler，用于输出到控制台
+    console_handler.setFormatter(formatter)  # 设置控制台日志格式
+
+    logger = logging.getLogger(name)
+    logger.setLevel(level)  # 设置日志级别
+
+    logger.addHandler(handler)  # 添加文件处理器
+    logger.addHandler(console_handler)  # 添加控制台处理器
+
+    return logger
